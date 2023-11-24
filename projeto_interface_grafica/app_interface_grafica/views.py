16e0fba658e7d3e91a87101b02721ea84f3e35a6
João Nasser <147288822+JNasser-Filho@users.noreply.github.com>
2023-11-24 12:51:34 -0300

    Enviando informações das teclas para o Django

--- projeto_interface_grafica/app_interface_grafica/views.py
+++ projeto_interface_grafica/app_interface_grafica/views.py
@@ -1,17 +1,13 @@
-'''from django.shortcuts import render
-
-def home(request):
-    return render(request,'usuarios/home.html')'''
-   
 import cv2
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from django.conf import settings
 import os
+from django.http import JsonResponse
 
 def obter_frames_camera():
     video = cv2.VideoCapture()
-    ip_camera = "https://192.168.101.2:8080/video"
+    ip_camera = "https://192.168.0.179:8080/video"
     video.open(ip_camera)
 
     while True:
@@ -22,7 +18,14 @@ def obter_frames_camera():
                b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n\r\n')
 
 def exibir_pagina_inicial(request):
-        return render(request, os.path.join(settings.BASE_DIR, 'app_interface_grafica', 'templates', 'home.html'))
+    return render(request, 'home.html')  # ajuste o caminho do arquivo HTML conforme a estrutura do seu projeto
 
 def obter_video(request):
     return StreamingHttpResponse(obter_frames_camera(), content_type="multipart/x-mixed-replace;boundary=frame")
+
+def receber_teclas(request):
+    if request.method == 'POST':
+        dados = request.POST.get('teclas')
+        # Faça o que quiser com os dados, como salvá-los no banco de dados
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'})
--- projeto_interface_grafica/projeto_interface_grafica/urls.py
+++ projeto_interface_grafica/projeto_interface_grafica/urls.py
@@ -1,14 +1,8 @@
-'''from django.urls import path
-from app_interface_grafica import views
-
-urlpatterns = [
-    path('',views.home,name="home"),
-]'''
-
 from django.urls import path
-from app_interface_grafica.views import exibir_pagina_inicial, obter_video
+from app_interface_grafica.views import exibir_pagina_inicial, obter_video, receber_teclas
 
 urlpatterns = [
     path('', exibir_pagina_inicial, name='pagina_inicial'),
     path('obter_video/', obter_video, name='obter_video'),
+    path('registrar-tecla/', receber_teclas, name='registrar_tecla'),
 ]
